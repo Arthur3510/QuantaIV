1968a89e25abe24be3b6880c0f3d013a9294ca51
Arthur3510 <143388385+Arthur3510@users.noreply.github.com>
2025-06-17 22:29:39 +0800

    Create indicator_utils.py

---
+++ utils/indicator_utils.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def calc_rsi(close, period):
+    delta = close.diff()
+    gain = (delta.where(delta > 0, 0)).rolling(window=period).mean()
+    loss = (-delta.where(delta < 0, 0)).rolling(window=period).mean()
+    rs = gain / loss
+    rsi = 100 - (100 / (1 + rs))
+    return rsi 
